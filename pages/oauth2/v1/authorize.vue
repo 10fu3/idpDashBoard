9a8f9c595bb07b4678915e3322b7f2614b5e8fd3
10fu3 <msf.1192@gmail.com>
2021-02-04 23:55:39 +0900

    fix constant variable

--- pages/oauth2/v1/authorize.vue
+++ pages/oauth2/v1/authorize.vue
@@ -132,8 +132,7 @@
         },
         methods: {
             login: function () {
-                var a = location.href.replace(String(document.URL.split(":")[0]+"://"+document.domain),"");
-                fetch(this.api_server+String, { 
+                fetch(this.api_server+location.href.replace(String(document.URL.split(":")[0]+"://"+document.domain),""), { 
                     method: 'post',
                     headers: {
                         'Content-Type': 'text/plain;charset=UTF-8',
